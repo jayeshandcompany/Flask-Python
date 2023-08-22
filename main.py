e6ea70e31541aa6368e852b91366c0047115dd84
jayeshandcompany <63994522+jayeshandcompany@users.noreply.github.com>
2023-08-22 06:06:13 +0000

    commit 2

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-from flask import Flask, request, render_template
+from flask import Flask, flash, request, render_template
+from werkzeug.utils import secure_filename
 
 app = Flask(__name__)
+app.secret_key = "8t872tr13cucsjcsco98*&^%$#@s"
 
 
 @app.route("/")
@@ -15,20 +17,28 @@ def path(subpath):
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
-  if request.method == 'GET':
-    return dologin()
+  error = None
   if request.method == "POST":
-    return loggedin()
+    if request.form['user'] != 'admin' or request.form['pass'] != 'try':
+      error = "invalid credentials"
+    else:
+      flash("hogya bhai")
+      user = request.form['user']
+      return loggedin(user)
+  return render_template('login.html', error=error)
+
+
 @app.route('/hello/<string:name>')
 def helo(name='jayesh'):
-  return render_template('try.html.jinja',name=name)
+  return render_template('try.html.jinja', name=name)
+
 
 def dologin():
   return render_template('login.html')
 
 
-def loggedin():
-  return render_template('logged.html')
+def loggedin(user):
+  return render_template('logged.html', username=user)
 
 
 if __name__ == "__main__":
